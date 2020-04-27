24a6324c6f52d079818e8a9d5e56f3a128283173
me <me@me.me.com>
2020-04-27 14:17:48 -0700

    finish up the project

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
   $("form").submit(function(e){
     e.preventDefault();
-    
+    clear();
+    $("input[name=work]:checked").each(function(item){
+        $("#worklist").append("<li>"+ $(this).val() +"</li>");
+    });
+
+    $("input[name=nonwork]:checked").each(function(item){
+      $("#nonworklist").append("<li>"+ $(this).val() +"</li>");
+  });
+
+
+    function clear(){
+      $("li").remove();
+    }
   })
 });
